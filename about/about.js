8c31cc207b0b876b6a6247ed735d0e5d4e30796e
karenfann <kfann285@gmail.com>
2017-11-20 01:08:17 -0800

    add animation to about page

--- about/about.js
+++ about/about.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('.statement').hide().delay(300).slideDown(500)
+  $('.statement').hide().delay(500).slideDown(500)
   $('.info-wrapper').hide()
   $('.member').on('mouseover', function() {
     let id = '#' + $(this).data('id')
